530b0974da10482b31fb60462b3dcc06bdbba347
E14mayam <93450333+E14mayam@users.noreply.github.com>
2022-04-09 15:31:59 +0100

    smoothscroll function implemented

--- main.js
+++ main.js
@@ -1,23 +1,9 @@
-$(function () {
-    // This will select everything with the class smoothScroll
-    // This should prevent problems with carousel, scrollspy, etc...
-    $(".smoothScroll").click(function () {
-      if (
-        location.pathname.replace(/^\//, "") ==
-          this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-        if (target.length) {
-          $("html,body").animate(
-            {
-              scrollTop: target.offset().top
-            },
-            1000
-          ); // The number here represents the speed of the scroll in milliseconds
-          return false;
-        }
-      }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
